bdaba3526542a1a8eaddd24ae523362aea0ae84b
Charlotte Collins <charlottecollins@Charlottes-MacBook-Pro-2.local>
2023-09-14 16:36:11 -0500

    Added program that computes volume of a sphere

---
+++ volume_of_sphere.py
@@ -0,0 +1,14 @@
+# This program computes area of a circle
+def compute_area_of_Sphere(radius):
+	pi = 3.14
+	area = 4 * pi * radius * radius
+	return area
+
+radius1 = 30
+area1 = compute_area_of_sphere(radius1)
+print(f"The area of circle with sphere {radius1} is: {area1}")
+
+radius2 = 40
+area2 = compute_area_of_sphere(radius2)
+print(f"The area of circle with sphere {radius2} is: {area2}")
+
